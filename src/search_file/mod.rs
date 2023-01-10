cbb698cf5584fb56f1ecfae1391f212e2f724bcb
UnknowEntity <theplayer357@gmail.com>
2023-01-10 20:31:12 +0700

    fix: not search hidden file

--- src/search_file/mod.rs
+++ src/search_file/mod.rs
@@ -1,6 +1,7 @@
-use std::{path::Path, fs::DirEntry, env};
+use std::{path::Path, fs::{DirEntry, self}, env, os::windows::prelude::*};
 
 const NODE_MODULE: &'static str = "node_modules";
+const ATTR_HIDDEN: u32 = 0x2;
 
 fn is_match_name_dir(dir: &DirEntry) -> bool {
     let path = dir.path();
@@ -22,7 +23,22 @@ fn search_folder(path: &Path) -> Vec<DirEntry> {
         Err(_) => return vec![],
     };
 
-    let child_dir: Vec<DirEntry> = entries.filter_map(|entry| entry.ok()).filter(|entry| entry.path().is_dir()).collect();
+    let child_dir: Vec<DirEntry> = entries.filter_map(|entry| entry.ok()).filter(|entry| {
+        let is_hidden = if cfg!(target_os = "windows") {
+            let metadata = match fs::metadata(entry.path()) {
+                Ok(metadata) => metadata,
+                Err(_) => return false,
+            };
+
+            let attributes = metadata.file_attributes();
+
+            (attributes & ATTR_HIDDEN) > 0
+        } else {
+            false
+        };
+        entry.path().is_dir() && !is_hidden
+    }).collect();
+
     if let Some(dir_index) = child_dir.iter().position(|dir| is_match_name_dir(dir)) {
         let child = child_dir.into_iter().nth(dir_index).unwrap();
         return vec![child];
