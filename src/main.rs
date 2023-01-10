37720e7c327b7961d95e837b432f750c5b66a929
UnknowEntity <theplayer357@gmail.com>
2023-01-10 20:59:57 +0700

    fix: uncomment code

--- src/main.rs
+++ src/main.rs
@@ -13,17 +13,12 @@ async fn main() {
     let node_module_paths = get_files_path().await;
 
     for entry in node_module_paths {
-        // let node_module_size = Byte::from_bytes(file_helper::get_size(&entry.path()).await.into());
-        // println!(
-        //     "{}: {}",
-        //     entry.path().display().to_string(),
-        //     node_module_size.get_appropriate_unit(true)
-        // )
-
+        let node_module_size = Byte::from_bytes(file_helper::get_size(&entry.path()).await.into());
         println!(
-                "{}",
-                entry.path().display().to_string(),
-            )
+            "{}: {}",
+            entry.path().display().to_string(),
+            node_module_size.get_appropriate_unit(true)
+        )
     }
 
     println!("Time to execute: {:?}", start.elapsed())
