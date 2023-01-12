895eaa4ac2792cbb9d78988a73f219b9b4cb1410
UnknowEntity <theplayer357@gmail.com>
2023-01-12 20:37:08 +0700

    Revert "fix: format file struct"
    This reverts commit ab6e42554d6106511e2c35493e477766a75ce943.

--- src/file_helper/mod.rs
+++ src/file_helper/mod.rs
@@ -1,5 +1,47 @@
-mod cal_file_size;
-mod search_file;
+use std::{fs::DirEntry, io::Error, path::Path};
 
-pub use cal_file_size::*;
-pub use search_file::*;
+use tokio::task::JoinSet;
+
+fn cal_size_spawn(entry: Result<DirEntry, Error>, set: &mut JoinSet<u64>) {
+    set.spawn(async move {
+        let entry = match entry {
+            Ok(data) => data,
+            Err(_) => {
+                return 0;
+            }
+        };
+
+        if entry.path().is_dir() {
+            return get_size(entry.path().as_path()).await;
+        }
+
+        let child_size = match entry.metadata() {
+            Ok(data) => data,
+            Err(_) => {
+                return 0;
+            }
+        };
+
+        child_size.len()
+    });
+}
+
+pub async fn get_size(path: &Path) -> u64 {
+    let mut size = 0;
+    let children = match path.read_dir() {
+        Ok(children) => children,
+        Err(_) => return 0,
+    };
+
+    let mut set = JoinSet::new();
+
+    for entry in children .into_iter(){
+        cal_size_spawn(entry, &mut set);
+    }
+
+    while let Some(result) = set.join_next().await {
+        size += result.unwrap_or(0);
+    }
+
+    return size;
+}
