72596a7ceb7ab415504f5e08f9c211920730ae4a
UnknowEntity <theplayer357@gmail.com>
2023-01-12 21:09:00 +0700

    Revert "Revert "Revert "fix: format file struct"""
    This reverts commit 3516db4edabcc3a4f7d0e24d2ff53a9548b9f1b7.

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,6 @@ use std::time::Instant;
 use byte_unit::Byte;
 use search_file::get_files_path;
 
-use crate::file_helper::get_size;
-
 #[tokio::main]
 async fn main() {
     let start = Instant::now();
@@ -15,7 +13,7 @@ async fn main() {
     let node_module_paths = get_files_path().await;
 
     for entry in node_module_paths {
-        let node_module_size = Byte::from_bytes(get_size(&entry.path()).await.into());
+        let node_module_size = Byte::from_bytes(file_helper::get_size(&entry.path()).await.into());
         println!(
             "{}: {}",
             entry.path().display().to_string(),
